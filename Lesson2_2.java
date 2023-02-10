04da96dae9e50201776eed3c9685895a64973cf3
Tatiana Korysheva <korosteleva87@gmail.com>
2023-02-10 21:48:18 +0300

    initional commit

---
+++ Lesson2_2.java
@@ -0,0 +1,18 @@
+public class Lesson2_2 {
+
+    public static void main(String[] args) {
+        int startBalance = 50;
+        int addition = 1001;
+        int bonus = 0;
+
+        if (addition > 1000) {
+            bonus = addition / 100;
+        }
+        int totalBalanceWithBonus = startBalance + addition + bonus;
+
+        System.out.println("Итоговый счет " + totalBalanceWithBonus);
+        System.out.println("Итоговый бонус " + bonus);
+
+
+    }
+}
